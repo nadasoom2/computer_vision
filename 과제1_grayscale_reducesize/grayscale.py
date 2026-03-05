7c4066a18b210b3762e4db159a5b7d490b1d71dd
nadasoom2 <qazx3830@gmail.com>
2026-03-05 16:23:50 +0900

    renamed img and modified grayscale.py

--- 과제1_grayscale_reducesize/grayscale.py
+++ 과제1_grayscale_reducesize/grayscale.py
@@ -15,21 +15,20 @@ if img is None:
 #기본 이미지 줄이기
 img_small=cv.resize(img, dsize=(0,0), fx=0.5, fy=0.5)
 
-#--------------img_small을 grayscale로 변환-----------
+#----------img_small을 grayscale로 변환-----------
 gray = cv.cvtColor(img_small, cv.COLOR_BGR2GRAY)
 
-#---------------이미지 저장--------------
-cv.imwrite('soccer_small.jpg', img_small)
-cv.imwrite('soccer_gray.jpg', gray)
-
-#-------hstack을 위해 다시 3채널로 변환------
+#----------hstack을 위해 다시 3채널로 변환-----------
 gray_bgr = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
 
-#-------원본 + grayscale 가로 결합-------
+#-----------원본 + grayscale 가로 결합-----------
 combined = np.hstack((img_small, gray_bgr))
 
+# ------------최종 이미지 저장------------
+cv.imwrite('Combined_Image.jpg', combined)
 cv.imshow('Combined Image', combined)
 
+#아무 키나 누르면 창이 닫히도록
 cv.waitKey()
 cv.destroyAllWindows()
 
