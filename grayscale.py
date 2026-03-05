b00bf7d3894b28e8e5de0f997146fcdcec814339
nadasoom2 <qazx3830@gmail.com>
2026-03-05 16:09:26 +0900

    Modified grayscale.py

--- grayscale.py
+++ grayscale.py
@@ -1,22 +1,26 @@
+#opencv 불러오기
 import cv2 as cv
+#인터픠터 제어 sys라이브러리 불러오기
 import sys
-
-#np.hstack을 만들기 위해 numpy라이브러리 불러오기
+#np.hstack을 위해 numpy라이브러리 불러오기
 import numpy as np
 
+#이미지 불러오기
 img = cv.imread('soccer.jpg')
 
+#예외처리(이미지가 없다면 print문 출력)
 if img is None:
     sys.exit('파일이 존재하지 않습니다.')
 
+#기본 이미지 줄이기
 img_small=cv.resize(img, dsize=(0,0), fx=0.5, fy=0.5)
-#--------------grayscale 변환--------------
+
+#--------------img_small을 grayscale로 변환-----------
 gray = cv.cvtColor(img_small, cv.COLOR_BGR2GRAY)
 
-
-cv.imwrite('soccer_gray.jpg', gray)
+#---------------이미지 저장--------------
 cv.imwrite('soccer_small.jpg', img_small)
-
+cv.imwrite('soccer_gray.jpg', gray)
 
 #-------hstack을 위해 다시 3채널로 변환------
 gray_bgr = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
